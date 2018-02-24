ca9907768dcb76fefcef451e99ed6d65738f4433
rpalkar <rpalkar@purdue.edu>
2018-02-24 04:11:55 -0500

    Refined Table UI

--- GitHubSearchMadeEasy/ui.R
+++ GitHubSearchMadeEasy/ui.R
@@ -14,7 +14,11 @@ shinyUI(fluidPage(
     ),
     
     mainPanel(
-      tableOutput('results')
+      wellPanel(
+        tableOutput('results'),
+        style = "overflow-y:scroll; max-height: 500px;
+                       overflow-x:scroll; auto"
+      )
     )
   )
 ))
